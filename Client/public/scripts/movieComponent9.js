71fe8635b1206c5d9d4d90fad2b56b8fb07e47cb
srinivasaraokandregula <seenukandregula@gmail.com>
2016-06-13 11:01:17 +0000

    React MoviesList Complete

---
+++ Client/public/scripts/movieComponent9.js
@@ -0,0 +1,523 @@
+var {Router, Route, IndexRoute, Link, browserHistory } = ReactRouter
+
+var MainLayout = React.createClass({
+  render: function () {
+    return(
+      <div className="container" id="main">
+        <NavigationBar />
+        <main>
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
+});
+
+var NavigationBar = React.createClass({
+  render: function () {
+    return(
+      <div className="navbar navbar-fixed-top">
+        <div classname="container" id="navbar">
+          <div className="navbar-collapse collapse navbar-responsive-collapse">
+            <ul className="nav navbar-nav" id="navbar-list">
+              <li className="active"><Link to="/home">Home</Link></li>
+              <li><Link to="/movies">Movies</Link></li>
+              <li><Link to="/addmovie">Add Movie</Link></li>
+              <li><Link to="/contact">Contact Us</Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Home = React.createClass({
+  render: function () {
+    return(
+      <div className="row" id="homeBox">
+        <div className="col-md-12">
+          <div className="jumbotron">
+            <h2>Welcome!!</h2>
+            <p>Movies to Watch before You DIE</p>
+            <p>
+              You will find the list here &nbsp;
+              <Link to="/movies" className="btn btn-primary">Click Here</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+var MainBox = React.createClass({
+
+  getInitialState: function () {
+    return {
+      movieJSONArray: []
+    };
+  },
+
+  loadMovie: function(){
+    $.get('/api/movies', function (result) {
+
+    this.setState({  movieJSONArray: result
+              });
+      }.bind(this));
+  },
+
+  componentDidMount: function () {
+    this.loadMovie();
+    setInterval(this.loadMovie, 6000);
+  },
+
+  render: function () {
+    return(
+      <div className="container" id="mainBox">
+        <SearchBox />
+        <MovieBox jsonData = {this.state.movieJSONArray} movieWillDelete={this.loadMovie} />
+      </div>
+    );
+  }
+});
+
+var SearchBox = React.createClass({
+  getInitialState: function () {
+    return {
+      searchText: ''
+    };
+  },
+
+  componentDidMount: function () {
+
+    $('#searchSave').on('click', function(){
+      $.ajax({
+        type:'POST',
+        url: '/api/movies',
+        data: jQuery.param({Title: $('#searchBox').val()}),
+        cache: false,
+        success: function (data) {
+
+        }
+      });
+    });
+  },
+
+  render: function () {
+    return(
+      <div className="row wrap-content">
+        <form role="form">
+          <input id="searchBox" type="text" name="Title" className="form-control" placeholder="Enter Movie Name" />
+        </form>
+        <button id="searchSave" className="btn btn-primary form-control">Search and Save</button>
+      </div>
+    );
+  }
+});
+
+var MovieBox = React.createClass({
+  handleDelete: function (movie_ID) {
+    $.ajax({
+      type: 'DELETE',
+      url: '/api/movies/' + movie_ID,
+      success: function (result) {
+        return this.props.movieWillDelete();
+      }.bind(this)
+    })
+  },
+
+  render: function () {
+    var movierows = [];
+    this.props.jsonData.forEach(function (movie){
+       movierows.push(<Movie key={movie.imdbID} movieObj={movie} movieID={movie._id} onChooseDelete={this.handleDelete} />);
+     }.bind(this));
+
+    return(<div>{movierows}</div>);
+  }
+});
+
+var Movie = React.createClass({
+  getInitialState: function () {
+    var movieObj = this.props.movieObj;
+    return movieObj;
+  },
+
+  handleDeleteClick: function () {
+    var movie_ID = this.props.movieID;
+    return this.props.onChooseDelete(movie_ID);
+  },
+
+  render: function(){
+      return(
+        <div className="row wrap-content" id="movieElement">
+          <div className="col-md-4">
+             <img id="poster" alt="Bootstrap Image Preview" src={this.props.movieObj.Poster} />
+          </div>
+          <div className="col-md-8">
+            <div className="list-group" id="list-group">
+              <div className="list-group-item" id="list-group-item">
+                <ul className="list-unstyled" id="ulist">
+                  <li id="name">
+                    <span>Title :  </span> {this.props.movieObj.Title}
+                  </li>
+
+                  <li id="year">
+                    <span>Year :  </span> {this.props.movieObj.Year}
+                  </li>
+
+                  <li id="actors">
+                    <span>Actors :  </span> {this.props.movieObj.Actors}
+                  </li>
+
+                  <li id="director">
+                    <span>Director :  </span> {this.props.movieObj.Director}
+                  </li>
+
+                  <li id="genre">
+                    <span>Genre :  </span> {this.props.movieObj.Genre}
+                  </li>
+
+                  <li id="plot">
+                    <span>Plot :  </span> {this.props.movieObj.Plot}
+                  </li>
+
+                  <li id="released">
+                    <span>Released :  </span> {this.props.movieObj.Released}
+                  </li>
+
+                  <li id="rating">
+                    <span>Rating :  </span> {this.props.movieObj.imdbRating}
+                  </li>
+                </ul>
+                <input type="text" id="movieID" value={this.props.movieObj._id} hidden readOnly />
+                <div className="buttons">
+                  <a className="btn btn-primary" role="button" href={'http://www.imdb.com/title/' + this.props.movieObj.imdbID}  target="_blank"> View on IMDB </a>
+                  &nbsp; &nbsp; &nbsp;
+                  <Link to={'/updateMovie/'+this.props.movieObj._id} className="btn btn-warning" > Update </Link>
+                  <button className="btn btn-danger" id="deleteMovie" onClick={this.handleDeleteClick} >Delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+});
+
+var AddMovieBox = React.createClass({
+  componentDidMount: function(){
+    $('#addMovieButton').on('click', function () {
+
+      $.ajax({
+        type: 'POST',
+        url: 'api/movies/add',
+        data: $('#addForm').serialize() + '&addPoster=' + $('#addPoster').val(),
+        cache: false,
+        success: function () {
+          alert("Your Movie has been Added");
+        }
+      })
+    });
+  },
+
+  render: function () {
+    return(
+      <div className="row" id="addMovieBox">
+        <div className="col-md-3 pull-left"></div>
+    		<div className="col-md-6 jumbotron">
+    			<form role="form" id="addForm">
+            <div className="form-group">
+
+              <label for="addTitle">
+                Title
+              </label><br/>
+              <input type="text" className="form-control" id="addTitle" name="addTitle" />
+
+            </div>
+            <div className="form-group">
+
+              <label for="addYear">
+                Year
+              </label>
+              <input type="text" className="form-control" id="addYear" name="addYear" />
+
+            </div>
+    				<div className="form-group">
+
+    					<label for="addActors">
+    						Actors
+    					</label>
+    					<input type="text" className="form-control" id="addActors" name="addActors" />
+
+    				</div>
+    				<div className="form-group">
+
+              <label for="addDirector">
+                Director
+              </label>
+              <input type="text" className="form-control" id="addDirector" name="addDirector" />
+
+    				</div>
+            <div className="form-group">
+
+              <label for="addGenre">
+                Genre
+              </label>
+              <input type="text" className="form-control" id="addGenre" name="addGenre" />
+
+    				</div>
+            <div className="form-group">
+
+    					<label for="addPlot">
+    						Plot
+    					</label>
+    					<input type="text" className="form-control" id="addPlot" name="addPlot" />
+
+    				</div>
+            <div className="form-group">
+
+    					<label for="addReleased">
+    						Released
+    					</label>
+    					<input type="text" className="form-control" id="addReleased" name="addReleased" />
+
+    				</div>
+    				<div className="form-group">
+
+              <label for="addRating">
+                Rating
+              </label>
+              <input type="text" className="form-control" id="addRating" name="addRating" />
+
+    				</div>
+            <div className="form-group">
+
+              <label for="addID">
+                Movie ID
+              </label>
+              <input type="text" className="form-control" id="addID" name="addID" />
+
+    				</div>
+            <div className="form-group">
+
+              <label for="addPoster">
+                Upload Poster
+              </label>
+              <input type="file" className="form-control" id="addPoster" name="addPoster" />
+
+    				</div>
+    			</form>
+          <center>
+          <button className="btn btn-primary" id="addMovieButton">
+            Add New Movie
+          </button>
+          </center>
+    		</div>
+        <div className="col-md-3"></div>
+    	</div>
+    );
+  }
+
+});
+
+var UpdateMovieBox = React.createClass({
+  getInitialState: function () {
+    return {
+      updateMovieObj: [],
+      Title: '',
+      Year: '',
+      Actors: '',
+      Director: '',
+      Genre: '',
+      Released: '',
+      Plot: '',
+      imdbRating: '',
+      Poster: ''
+    };
+  },
+
+  componentDidMount: function () {
+
+    $.ajax({
+      type: 'GET',
+      url: '/api/movies/' + this.props.params.movie_id,
+      cache: false,
+      success: function (result) {
+        this.setState({updateMovieObj: result});
+        this.setState({Title: result.Title});
+        this.setState({Year: result.Year});
+        this.setState({Actors: result.Actors});
+        this.setState({Director: result.Director});
+        this.setState({Genre: result.Genre});
+        this.setState({Released: result.Released});
+        this.setState({Plot: result.Plot});
+        this.setState({imdbRating: result.imdbRating});
+        this.setState({Poster: result.Poster});
+
+      }.bind(this)
+
+    })
+
+  },
+
+  handleYearChange: function (e) {
+    this.setState({Year: e.target.value })
+  },
+
+  handleActorsChange: function (e) {
+    this.setState({Actors: e.target.value })
+  },
+
+  handleDirectorChange: function (e) {
+    this.setState({Director: e.target.value })
+  },
+
+  handleGenreChange: function (e) {
+    this.setState({Genre: e.target.value })
+  },
+
+  handleReleasedChange: function (e) {
+    this.setState({Released: e.target.value })
+  },
+
+  handlePlotChange: function (e) {
+    this.setState({Plot: e.target.value })
+  },
+
+  handleimdbRatingChange: function (e) {
+    this.setState({imdbRating: e.target.value })
+  },
+
+  handleUpdateMovieButtonClick: function () {
+
+
+      $.ajax({
+        type: 'PUT',
+        url: 'api/movies/' + this.props.params.movie_id,
+        data: $('#updateForm').serialize(),
+        cache: false,
+        success: function () {
+          alert("Your Movie has been Updated");
+        }
+      })
+
+  },
+
+  render: function () {
+    return(
+      <div className="row" id="updateMovieBox">
+        <div className="col-md-3 pull-left"></div>
+    		<div className="col-md-6 jumbotron">
+    			<form role="form" id="updateForm">
+            <div className="form-group">
+
+              <label for="updateTitle">
+                Title
+              </label><br/>
+              <input type="text" className="form-control" id="updateTitle" name="updateTitle"  value={this.state.Title} readOnly />
+
+            </div>
+            <div className="form-group">
+
+              <label for="updateYear">
+                Year
+              </label>
+              <input type="text" className="form-control" id="updateYear" name="updateYear" value={this.state.Year} onChange={this.handleYearChange} />
+
+            </div>
+    				<div className="form-group">
+
+    					<label for="updateActors">
+    						Actors
+    					</label>
+    					<input type="text" className="form-control" id="updateActors" name="updateActors" value={this.state.Actors} onChange={this.handleActorsChange} />
+
+    				</div>
+    				<div className="form-group">
+
+              <label for="updateDirector">
+                Director
+              </label>
+              <input type="text" className="form-control" id="updateDirector" name="updateDirector" value={this.state.Director} onChange={this.handleDirectorChange} />
+
+    				</div>
+            <div className="form-group">
+
+              <label for="updateGenre">
+                Genre
+              </label>
+              <input type="text" className="form-control" id="updateGenre" name="updateGenre" value={this.state.Genre} onChange={this.handleGenreChange} />
+
+    				</div>
+            <div className="form-group">
+
+    					<label for="updatePlot">
+    						Plot
+    					</label>
+    					<input type="text" className="form-control" id="updatePlot" name="updatePlot" value={this.state.Plot} onChange={this.handlePlotChange} />
+
+    				</div>
+            <div className="form-group">
+
+    					<label for="updateReleased">
+    						Released
+    					</label>
+    					<input type="text" className="form-control" id="updateReleased" name="updateReleased" value={this.state.Released} onChange={this.handleReleasedChange} />
+
+    				</div>
+    				<div className="form-group">
+
+              <label for="updateimdbRating">
+                Rating
+              </label>
+              <input type="text" className="form-control" id="updateimdbRating" name="updateimdbRating" value={this.state.imdbRating} onChange={this.handleimdbRatingChange} />
+
+    				</div>
+    			</form>
+          <center>
+          <button className="btn btn-primary" id="updateMovieButton" onClick={this.handleUpdateMovieButtonClick}>
+            Update Movie
+          </button>
+          </center>
+    		</div>
+        <div className="col-md-3"></div>
+    	</div>
+    );
+  }
+
+});
+
+var ContactBox = React.createClass({
+
+  render: function () {
+    return (
+      <div className="row" id="updateMovieBox">
+        <div className="col-md-3 pull-left"></div>
+    		<div className="col-md-6 jumbotron">
+          <h2>Contact</h2>
+          <p>You can contact us at : </p>
+          <p>Email : movieslist@imdb.com</p>
+          <p>Blog : movieslist@blogspot.com</p>
+    		</div>
+        <div className="col-md-3"></div>
+    	</div>
+    );
+  }
+});
+
+var browserHistory = ReactRouter.browserHistory;
+ReactDOM.render(
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={MainLayout}>
+        <IndexRoute component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/movies" component={MainBox} />
+        <Route path="/addmovie" component={AddMovieBox} />
+        <Route path="/contact" component={ContactBox} />
+        <Route path="/updateMovie/:movie_id" component={UpdateMovieBox} />
+      </Route>
+    </Router>
+  ), document.getElementById('content')
+);
